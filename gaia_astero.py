cac88d63e1f58021e7d13025d1368feaaf5dae92
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2016-10-16 19:03:39 -0400

    adding gaia_astero

---
+++ gaia_astero.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+
+
+class gaia_astero(object):
+        """
+        Method for querying a star to see if has any asteroseismic data.
+        The star object has KIC, 2MASS and tycho ids, stellar parameters,
+        asteroseismic parameters and gaia parameters.
+
+        get_all_astero() returns a database of all gaia stars with
+        asteroseismology.
+        """
+
+        def __init__(self):
+
+            rgs = pd.read_csv("stello_2013_tgas_xmatch.csv")
+            self.redgiant = rgs
+
+            sls = pd.read_csv("solar-like.csv")
+            self.solarlike = sls
+
+            # self.all_stars = ("all_stars.csv")
+
+        def star(self, id):
+            """
+            Return the properties of a single star.
+            id should be a Kepler id.
+            """
+            if len(str(id)) > 9:
+                id_type = "tm_designation"
+                id = str
+            else:
+                id_type = "kepid"
+
+            m = id == self.solarlike[id_type]
+            sl = self.solarlike.kepid[m]
+            if len(sl[m]):
+                print("Solar-like star")
+                return self.solarlike.iloc[np.where(m)[0][0]]
+
+            m = id == self.solarlike[id_type]
+            rg = self.redgiant.kepid[m]
+            if len(rg[m]):
+                print("Red Giant")
+                return self.redgiant.iloc[np.where(m)[0][0]]
+
+
+if __name__ == "__main__":
+    id = 6116048
+    ga = gaia_astero()
+    print(ga.star(id))
